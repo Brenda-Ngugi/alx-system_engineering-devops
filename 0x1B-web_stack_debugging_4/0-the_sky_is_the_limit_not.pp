9227a82cfafdf2af495a698f74f96004206c6a01
Brenda-Ngugi <125594650+Brenda-Ngugi@users.noreply.github.com>
2024-02-07 20:28:49 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# Increasing nginx requests handling using puppet
-exec {'requests_capacity':
-  environment => ['DIR=/etc/default/nginx', 'OLD=15', 'NEW=15000'],
-  command     => "sudo sed -i 's/\$OLD/\$NEW/' \$DIR; sudo service nginx restart",
-  path        => ['/usr/bin', '/bin'],
+# fix our stack so that we get to 0 errors
+exec { 'file limit':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  provider => shell,
 }
